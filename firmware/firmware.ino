0b337a0a8ebd24fa783dc9033c95703be9810a90
Phil Tsaryk <phil@tsaryk.com>
2020-03-31 00:21:37 +0200

    style(firmware): reformat with Prettier

--- firmware/firmware.ino
+++ firmware/firmware.ino
@@ -5,19 +5,24 @@ IRrecv irrecv(RECV_PIN);
 decode_results results;
 unsigned long key_value = 0;
 
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   irrecv.enableIRIn();
   irrecv.blink13(true);
 }
 
-void loop() {
-  if (irrecv.decode(&results)) {
-    if (results.value == 0xFFFFFFFF) {
+void loop()
+{
+  if (irrecv.decode(&results))
+  {
+    if (results.value == 0xFFFFFFFF)
+    {
       results.value = key_value;
     }
 
-    switch (results.value) {
+    switch (results.value)
+    {
     case 0x9E6140BF:
       Serial.println("play");
       break;
