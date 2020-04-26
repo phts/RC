0c8d450e48b071d2f16706aa142f3bb7a66e538e
Phil Tsaryk <phil@tsaryk.com>
2020-04-26 23:48:57 +0200

    fix(driver): make `mappings` required

--- driver/lib/settings.js
+++ driver/lib/settings.js
@@ -7,11 +7,13 @@ const rawSettings = require('cjson').load(SETTINGS_PATH)
 if (!rawSettings.serialPort) {
   throw new Error('serialPort is required')
 }
+if (!rawSettings.mappings) {
+  throw new Error('mappings is required')
+}
 
 const DEFAULT_SETTINGS = {
   debounceDelay: 150,
   initialState: null,
-  mappings: {},
   noDebounce: [],
 }
 
